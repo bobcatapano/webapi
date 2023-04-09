fb44f307c5dd99df9cadcd7ca9c23b2a9857250b
robcsensato <109102162+robcsensato@users.noreply.github.com>
2023-04-08 20:22:13 -0600

    completed code. Needs testing still.

--- webapi3/Controllers/AuthController.cs
+++ webapi3/Controllers/AuthController.cs
@@ -8,6 +8,7 @@ using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using System.Net.WebSockets;
 
 namespace webapi3.Controllers
 {
@@ -80,6 +81,44 @@ namespace webapi3.Controllers
                Status = "Success",
                Message = "User created Successfully!"
             });
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginVM loginVM)
+        {
+            var user = await userManager.FindByNameAsync(loginVM.UserName);
+            if (user != null && await userManager.CheckPasswordAsync(user, loginVM.Password)) {
+
+                var userRoles = await userManager.GetRolesAsync(user);
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(ClaimTypes.MobilePhone, user.PhoneNumber),
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                };
+                
+                foreach (var userRole in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+                }
+
+                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JsonWebTokenKeys:IssueSigningKry"]));
+                var token = new JwtSecurityToken(
+                    expires: DateTime.Now.AddHours(3),
+                    claims: authClaims,
+                    signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                    );
+                return Ok(new {
+                    api_key = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo,
+                    user = user,
+                    Role = userRoles,
+                    status = "User Login Successfully"
+                });
+            }
+            return Unauthorized();
         }
     }
 } 
